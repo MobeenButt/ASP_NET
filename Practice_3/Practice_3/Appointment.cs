403a66a7f37c386f946a79a21f78028bbce10472
MobeenButt <mobeen914butt@gmail.com>
2025-11-21 02:43:54 +0500

    HW_03

---
+++ Practice_3/Practice_3/Appointment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_3
+{
+    internal class Appointment
+    {
+        private int appointmentId;
+        private int doctorId;
+        private string? patientCnic;
+        private DateTime appointmentDate;
+        private int GenerateAppointmentID()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1000, 9999);
+        }
+        public Appointment(int doctorId,string patientCnic,DateTime date)
+        {
+            this.appointmentId = GenerateAppointmentID();
+            this.doctorId = doctorId;
+            this.patientCnic = patientCnic;
+            this.appointmentDate = date;
+        }
+        public int getAppointmentID { get=>appointmentId; }
+        public override string ToString()
+        {
+            return $"Appointment ID: {appointmentId}, Doctor ID: {doctorId}, Patient CNIC: {patientCnic}, Date: {appointmentDate}";
+        }
+
+    }
+}
---
+++ Practice_3/Practice_3/Doctor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_3
+{
+    internal class Doctor
+    {
+        private int doctorId;
+        private string ?name;
+        private string ?specialization;
+        private bool isAvailable;
+        private int GenerateDoctorID()
+        {
+            Random rnd=new Random();
+            return rnd.Next(1000,9999);
+        }
+        public Doctor(string name,string specialization)
+        {
+            this.doctorId = GenerateDoctorID();
+            this.name = name;
+            this.specialization = specialization;
+            this.isAvailable = true;
+        }
+        public int GetDoctorID { get=> doctorId; }
+        public string? GetName { get => name; set => name = value; }
+        public string? GetSpecialization { get=>specialization;set=>specialization= value; }
+        public bool IsAvailable { get=>isAvailable; set=>isAvailable=value; }
+        public void MarkUnavailable()
+        {
+            isAvailable = false;
+        }
+        public void MarkAvailable()
+        {
+            isAvailable = true;
+        }
+        public override string ToString()
+        {
+            return $"Doctor ID: {doctorId}, Name: {name}, Specialization: {specialization}, Available: {isAvailable}";
+        }
+
+    }
+}
