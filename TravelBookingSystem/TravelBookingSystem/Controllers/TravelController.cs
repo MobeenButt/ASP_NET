db93997dc2cbf9d348e28412e327079fac095bca
MobeenButt <mobeen914butt@gmail.com>
2025-11-29 01:25:09 +0500

    Travel Booking System - Added Models(classes), Services(Interface) then Interface Implementation(Completed)

---
+++ TravelBookingSystem/TravelBookingSystem/Controllers/TravelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelBookingSystem.Controllers
+{
+    public class TravelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TravelBookingSystem/TravelBookingSystem/Services/TravelService.cs
+++ TravelBookingSystem/TravelBookingSystem/Services/TravelService.cs
@@ -11,6 +11,7 @@ namespace TravelBookingSystem.Services
             string query = "Select * from TravelPackages";
             using(SqlConnection con=DbHelper.GetConnection())
             {
+                con.Open();
                 SqlCommand cmd = new SqlCommand(query, con);
                 SqlDataReader reader = cmd.ExecuteReader();
                 while(reader.Read())
@@ -40,7 +41,8 @@ namespace TravelBookingSystem.Services
         public TravelPackage GetPackageById(int id)
         {
             string query = "Select PackageId, PackageName, Destination, Duration,Price,AvailableSeats,DepartureDate from TravelPackages Where PackageId =@id ";
-            using(SqlConnection con = DbHelper.GetConnection()) { 
+            using(SqlConnection con = DbHelper.GetConnection()) {
+                con.Open();
                 SqlCommand cmd = new SqlCommand(query, con);
                 cmd.Parameters.AddWithValue("@id", id);
                 SqlDataReader reader = cmd.ExecuteReader();
@@ -74,10 +76,45 @@ namespace TravelBookingSystem.Services
        public bool CreateBooking(Booking booking)
         {
             //validation for bookings
+            if(booking==null)
+            {
+                throw new ArgumentNullException(nameof(booking));            }
             //Package check available or not 
+            var package = GetPackageById(booking.PackageID);
+            if (package == null)
+            {
+                throw new ArgumentException("Package not Found");
+            }
+            if(package.AvailableSeats<booking.NumberOfTravelers)
+            {
+                return false;
+            }
             //Insert booking
+           
+            using (SqlConnection con = DbHelper.GetConnection())
+            {
+                con.Open();
+                string insertQuery = "Insert into Bookings (PackageID,CustomerName,CustomerEmail,NumberOfTravelers,TotalAmount,BookingDate) Values (@PackageID,@CustomerName,@CustomerEmail,@NumberOfTravelers,@TotalAmount,@BookingDate)";
+                SqlCommand cmd = new SqlCommand(insertQuery,con);
+                cmd.Parameters.AddWithValue("@PackageID", booking.PackageID);
+                cmd.Parameters.AddWithValue("@CustomerName", booking.CustomerName);
+                cmd.Parameters.AddWithValue("@CustomerEmail", booking.CustomerEmail);
+                cmd.Parameters.AddWithValue("@NumberOfTravelers", booking.NumberOfTravelers);
+                cmd.Parameters.AddWithValue("@TotalAmount", booking.TotalAmount);
+                cmd.Parameters.AddWithValue("@BookingDate", booking.BookingDate);
+                cmd.ExecuteNonQuery();
+            }
             //update seats
-            throw new NotImplementedException();
+            using (SqlConnection con = DbHelper.GetConnection())
+            {
+                con.Open();
+                string updateQuery = "Update TravelPackages Set AvailableSeats=AvailableSeats - @NumberOfTravelers Where PackageID=@PackageID";
+                SqlCommand cmd = new SqlCommand(updateQuery, con);
+                cmd.Parameters.AddWithValue("@NumberOfTravelers", booking.NumberOfTravelers);
+                cmd.Parameters.AddWithValue("@PackageID", booking.PackageID);
+                cmd.ExecuteNonQuery();
+            }
+            return true;
         }
     }
 }
