21f0100092c42e017a5c9fbf47fa0fe9cbf043d5
MobeenButt <mobeen914butt@gmail.com>
2025-11-30 22:47:33 +0500

    Travel Booking System - Added View and Controllers

--- TravelBookingSystem/TravelBookingSystem/Controllers/TravelController.cs
+++ TravelBookingSystem/TravelBookingSystem/Controllers/TravelController.cs
@@ -4,9 +4,23 @@ namespace TravelBookingSystem.Controllers
 {
     public class TravelController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+
+        }
+        [HttpGet]
+        public IActionResult Book(int id) { 
+        }
+        [HttpPost]
+        public IActionResult Book()
+        {
+
         }
     }
 }
--- TravelBookingSystem/TravelBookingSystem/Program.cs
+++ TravelBookingSystem/TravelBookingSystem/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Travel}/{action=Index}/{id?}");
 
 app.Run();
