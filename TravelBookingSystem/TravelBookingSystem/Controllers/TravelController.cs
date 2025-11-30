a50b388ec5a26a6c748cffe02dc97bc1bb1f6bbe
MobeenButt <mobeen914butt@gmail.com>
2025-12-01 01:26:16 +0500

    Travel Booking System - Strongly-typed data(using ViewBag)

--- TravelBookingSystem/TravelBookingSystem/Controllers/TravelController.cs
+++ TravelBookingSystem/TravelBookingSystem/Controllers/TravelController.cs
@@ -1,26 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TravelBookingSystem.Models;
+using TravelBookingSystem.Services;
 
 namespace TravelBookingSystem.Controllers
 {
     public class TravelController : Controller
     {
+        public readonly ITravelService service;
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var packages = service.GetAllPackages();
+            return View(packages);
         }
         [HttpGet]
         public IActionResult Details(int id)
         {
-
+            var packageById = service.GetPackageById(id);
+            if (packageById == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Package = packageById;
+            return View();
         }
         [HttpGet]
-        public IActionResult Book(int id) { 
+        public IActionResult Book(int id)
+        {
+            var booking = service.GetPackageById(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            return View(booking);
         }
         [HttpPost]
-        public IActionResult Book()
+        public IActionResult Book(Booking model)
         {
-
+            //total amount
+            var pkgInfo=service.GetPackageById(model.PackageID);
+            model.TotalAmount = pkgInfo.Price * model.NumberOfTravelers;
+            bool success = service.CreateBooking(model);
+            if(success)
+            {
+                ViewBag.Message= "Booking Successful!";
+                ViewBag.Booking=model;
+                return View("Success");
+            }
+            ViewBag.Message= "Booking Failed. Please try again.";
+            ViewBag.Booking = model;
+            return View("Failure");
         }
     }
 }
