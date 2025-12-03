9e73c52145be96f4bae1607f5198265af15761a6
MobeenButt <mobeen914butt@gmail.com>
2025-12-04 00:52:56 +0500

    State Management through Cookies

--- State Management/State Management/Controllers/HomeController.cs
+++ State Management/State Management/Controllers/HomeController.cs
@@ -8,12 +8,35 @@ namespace State_Management.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            string data = String.Empty;
+            if (HttpContext.Request.Cookies.ContainsKey("first_request"))
+            {
+                string firstVisitedDaateTime = HttpContext.Request.Cookies["first_request"];
+                data = "Welcome Back"+ firstVisitedDaateTime;
+
+            }
+            else
+            {
+                //    var cookieOptions = new CookieOptions
+                //    {
+                //        Expires = DateTime.Now.AddDays(30), // Cookie expires in 30 days
+                //        HttpOnly = true,
+                //        Secure = true,
+                //        SameSite = SameSiteMode.Strict
+                //    };
+                CookieOptions option = new CookieOptions();
+                option.Expires = System.DateTime.Now.AddDays(1);
+                data = "you visited first time";
+
+                HttpContext.Response.Cookies.Append("first_request", System.DateTime.Now.ToString(),option);
+            }
+            return View("index",data);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Remove()
         {
-            return View();
+            HttpContext.Response.Cookies.Delete("first_request");
+            return View("index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
