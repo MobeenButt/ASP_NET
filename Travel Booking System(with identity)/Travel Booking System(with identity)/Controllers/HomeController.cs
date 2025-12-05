53753525ea994d48a6f4707ee77b4021289dc4a2
MobeenButt <mobeen914butt@gmail.com>
2025-12-06 03:12:57 +0500

    Travel Booking System (with identity)

---
+++ Travel Booking System(with identity)/Travel Booking System(with identity)/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Travel_Booking_System_with_identity_.Models;
+
+namespace Travel_Booking_System_with_identity_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Travel Booking System(with identity)/Travel Booking System(with identity)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Travel_Booking_System_with_identity_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
