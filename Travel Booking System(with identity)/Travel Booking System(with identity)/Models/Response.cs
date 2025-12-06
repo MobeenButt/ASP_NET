64ff57023c27d3ba4502ea506d3edd544110d61c
MobeenButt <mobeen914butt@gmail.com>
2025-12-07 02:34:56 +0500

    Travel Booking System (with identity not completed)

---
+++ Travel Booking System(with identity)/Travel Booking System(with identity)/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Travel_Booking_System_with_identity_.Models
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
---
+++ Travel Booking System(with identity)/Travel Booking System(with identity)/Models/TravelPackage.cs
@@ -0,0 +1,13 @@
+﻿namespace Travel_Booking_System_with_identity_.Models
+{
+    public class TravelPackage
+    {
+        public int PackageID { get; set; }
+        public string PackageName { get; set; }
+        public string Destination { get; set; }
+        public int DurationDays { get; set; }
+        public decimal Price { get; set; }
+        public int AvailableSeats { get; set; }
+        public DateTime DepartureDate { get; set; }
+    }
+}
