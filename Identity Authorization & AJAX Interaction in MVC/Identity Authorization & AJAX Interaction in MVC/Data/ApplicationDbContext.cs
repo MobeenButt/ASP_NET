a4ba754bfdb60c23c9cf207e928531975559088f
MobeenButt <mobeen914butt@gmail.com>
2025-12-13 23:04:17 +0500

    Identity Authorization & AJAX Interaction in MVC Applications

---
+++ Identity Authorization & AJAX Interaction in MVC/Identity Authorization & AJAX Interaction in MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_Authorization___AJAX_Interaction_in_MVC.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ Identity Authorization & AJAX Interaction in MVC/Identity Authorization & AJAX Interaction in MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Identity_Authorization___AJAX_Interaction_in_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
