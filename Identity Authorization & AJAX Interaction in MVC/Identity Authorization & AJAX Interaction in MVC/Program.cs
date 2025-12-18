38622c9334954d561dfd5de91bf619d8901f2e5c
MobeenButt <mobeen914butt@gmail.com>
2025-12-18 23:08:28 +0500

    Adding Identity

--- Identity Authorization & AJAX Interaction in MVC/Identity Authorization & AJAX Interaction in MVC/Program.cs
+++ Identity Authorization & AJAX Interaction in MVC/Identity Authorization & AJAX Interaction in MVC/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
